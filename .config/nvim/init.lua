82cf5fb4956d2a15ff39654c3d06d391c04f8942
Andreas Öhman <andreas.ohman@>
2023-03-12 00:14:54 +0100

    Add dynamic scaling

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -183,4 +183,16 @@ vim.api.nvim_set_keymap('n', 'gD', "<cmd>lua require('goto-preview').goto_previe
 if vim.g.neovide then
     vim.g.neovide_scroll_animation_length = 0.5
     vim.g.neovide_hide_mouse_when_typing = true
+
+    vim.g.neovide_scale_factor = 1.0
+
+    local change_scale_factor = function(delta)
+        vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+    end
+    vim.keymap.set("n", "<C-+>", function()
+        change_scale_factor(1.25)
+    end)
+    vim.keymap.set("n", "<C-->", function()
+        change_scale_factor(1/1.25)
+    end)
 end
