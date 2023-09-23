382beff1cb711eeb5a6e589e16c978419e824603
Andreas Öhman <andreas.ohman@>
2023-09-23 09:45:24 +0200

    Move rest of the keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -45,6 +45,7 @@ vim.o.foldenable = true
 
 vim.g.everforest_background = 'soft'
 set.background = 'light'
+set.grepprg = "rg --vimgrep --no-heading --smart-case"
 
 toggleDarkMode = function()
     if vim.opt.background:get() == 'dark' then
@@ -54,77 +55,9 @@ toggleDarkMode = function()
     end
 end
 
-local keymap = function(mode, lhs, rhs, opts)
-    opts = opts or { noremap = true, silent = true }
-    vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
-end
-
-set.grepprg = "rg --vimgrep --no-heading --smart-case"
-
 vim.cmd([[colorscheme everforest]])
  -- Hide the tildes that denotes end of file
 vim.cmd([[highlight! EndOfBuffer ctermbg=bg ctermfg=bg guibg=bg guifg=bg]])
-
-
--- Non leader key bindings
-keymap('n', 'gr',                    '<nop>')
-keymap('n', 'gr',                    '<cmd>Telescope lsp_references<cr>')
-keymap('x', '<leader>cl', ':Commentary<cr>')
-keymap('x', '<leader>ch', ':ClangdSwitchSourceHeader<cr>')
-
-
--- Tabs
-keymap('n', '<C-n>', '<CMD>tab new<cr><CMD>lua MiniStarter.open()<cr>', { noremap = true, silent = true })
-keymap('n', '<C-l>', '<CMD>tabn<cr>', { noremap = true, silent = true })
-keymap('n', '<C-h>', '<CMD>tabp<cr>', { noremap = true, silent = true })
-
-keymap('t', '<C-n>', '<CMD>tab new<cr>', { noremap = true, silent = true })
-keymap('t', '<C-l>', '<CMD>tabn<cr>', { noremap = true, silent = true })
-keymap('t', '<C-h>', '<CMD>tabp<cr>', { noremap = true, silent = true })
-
-keymap('n', '<C-t>', '<CMD>tab new<cr><CMD>term<cr>', { noremap = true, silent = true })
-keymap('t', '<C-t>', '<CMD>tab new<cr><CMD>term<cr>', { noremap = true, silent = true })
-
- -- LSP
-keymap('n', 'gd', ':lua vim.lsp.buf.definition()<cr>')
-keymap('n', 'gl', ':lua vim.diagnostic.open_float()<cr>')
-keymap('n', 'K', ':lua vim.lsp.buf.hover()<enter>')
-
- -- Terminal
-keymap('n', '<leader><tab>', '<cmd>ToggleTerm<cr>')
-keymap('t', '<leader><tab>', "<C-\\><C-n><cmd>ToggleTerm<cr>", { noremap = true, silent = true })
-
--- Remove search highlihgt with ESC
-keymap('n', '<ESC>', ':noh<CR><ESC>', { noremap = true, silent = true })
-
--- Aligning 
-keymap('n', 'ga', '<Plug>(EasyAlign)', { noremap = false, silent = true })
-keymap('x', 'ga', '<Plug>(EasyAlign)', { noremap = false, silent = true })
-
--- remove some unwanted bindings
-keymap('n', '<leader>hp', '<nop>')
-keymap('n', '<leader>hs', '<nop>')
-keymap('n', '<leader>hu', '<nop>')
-keymap('n', '<leader>nr', '<nop>')
-
--- Exit Terminal mode
-keymap('t', '<leader><Esc>', '<C-\\><C-n>')
-
-vim.g.copilot_no_tab_map = true
--- Uncomment to enable copilot autocompletion
-keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-
--- Nibbler
-keymap('n', '<C-b>', '<cmd>NibblerToggle<cr>')
-keymap('v', '<C-b>', '<cmd>NibblerToggle<cr>')
-keymap('n', '<C-g>', '<cmd>NibblerToBin<cr>')
-keymap('v', '<C-g>', '<cmd>NibblerToBin<cr>')
-
--- Goto 
-keymap('n', 'gD', "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", { noremap = true, silent = true })
-
-keymap('n', '<C-e>', '<C-e>j')
-keymap('n', '<C-y>', '<C-y>k')
 
 -- Fix for Cmp missing lsp completion
 local cmp = require('cmp')
@@ -146,16 +79,19 @@ cmp.setup({
 if vim.g.neovide then
     vim.g.neovide_scroll_animation_length = 0.5
     vim.g.neovide_hide_mouse_when_typing = true
-
     vim.g.neovide_scale_factor = 1.0
 
     local change_scale_factor = function(delta)
         vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
     end
-    vim.keymap.set("n", "<C-+>", function()
+
+    vim.keymap.set("n", "<C-+>", 
+    function()
         change_scale_factor(1.10)
     end)
-    vim.keymap.set("n", "<C-->", function()
+
+    vim.keymap.set("n", "<C-->", 
+    function()
         change_scale_factor(1/1.10)
     end)
     vim.o.guifont = "Iosevka"
