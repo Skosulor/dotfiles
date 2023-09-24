f336e9770aabe4026dd4db732eb66eefa23755e2
Andreas Öhman <andreas.ohman@>
2023-09-24 19:17:57 +0200

    Add nvim dap for debugger

---
+++ .config/nvim/lua/plugins/debugging.lua
@@ -0,0 +1,36 @@
+-- Using vscode extension for gdb
+-- Download: https://github.com/microsoft/vscode-cpptools/releases
+-- Unpack it. .vsix is a zip file and you can use unzip to extract the contents.
+-- Ensure extension/debugAdapters/bin/OpenDebugAD7 is executable.
+
+return {
+    {
+        'mfussenegger/nvim-dap',
+        config = function()
+            local dap = require('dap')
+
+            dap.adapters.lldb = {
+                type = 'server',
+                port = "1234",
+                executable = {
+                    command = '/opt/homebrew/opt/llvm/bin/lldb-vscode',
+                    args = {"--port", "1234"},
+                }
+            }
+
+            dap.configurations.c = {
+                {
+                    name = "Launch file",
+                    type = "lldb",
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                    end,
+                    cwd = '${workspaceFolder}',
+                    stopOnEntry = false,
+                },
+            }
+            end
+    },
+}
+
