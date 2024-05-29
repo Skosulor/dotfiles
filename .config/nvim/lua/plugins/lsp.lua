2879683690d8495ffed51d05154b4baac4f808b7
Andreas <hell@pop-os.lan>
2024-05-29 22:04:04 +0200

    u

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -7,7 +7,33 @@ return {
     'L3MON4D3/LuaSnip',
     {'williamboman/mason.nvim'},
     {'williamboman/mason-lspconfig.nvim'},
-    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {
+        'VonHeikemen/lsp-zero.nvim', 
+        branch = 'v3.x' ,
+        config = function ()
+            local lsp_zero = require('lsp-zero')
+
+            lsp_zero.on_attach(function(client, bufnr)
+                -- see :help lsp-zero-keybindings
+                -- to learn the available actions
+                lsp_zero.default_keymaps({buffer = bufnr})
+            end)
+
+            -- to learn how to use mason.nvim
+            -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
+            require('mason').setup({})
+            require('mason-lspconfig').setup({
+                ensure_installed = {},
+                handlers = {
+                    function(server_name)
+                        require('lspconfig')[server_name].setup({})
+                    end,
+                },
+            })
+        end
+        -- here yo can setup the language servers
+
+    },
     {'neovim/nvim-lspconfig'},
     {
         'hrsh7th/nvim-cmp',
